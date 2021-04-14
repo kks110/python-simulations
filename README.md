0f296e16ebd42fe087700236bb245113bd24e5cf
Kelvin Samuel <kelvin@kks110.com>
2021-04-15 00:16:55 +0100

    add readme and requierments

---
+++ README.md
@@ -0,0 +1,15 @@
+## Python Simulations
+
+A python script to run a few simulations.
+
+1 - A simulation of evolution
+2 - A simulation of natural selection
+
+To install the requirements, run
+```shell
+pip install -r requirements.txt
+```
+
+Run `main.py` and it will and it will ask which which sim you want to run.
+
+Heavily influenced by [Primer's](https://www.youtube.com/channel/UCKzJFdi57J53Vr_BkTfN3uQ) youtuber videos.
--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import natural_selection.run_natural_selection as natural_selection
 
 
 def start_up():
-    natural_selection.run()
-    # print("What would you like to run?")
-    # print("1: Evolution")
-    # print("2: Natural Selection")
-    # sim = input()
-    # if sim == "1":
-    #     evolution.run()
-    # if sim == "2":
-    #     natural_selection.run()
+    print("What would you like to run?")
+    print("1: Evolution")
+    print("2: Natural Selection")
+    sim = input()
+    if sim == "1":
+        evolution.run()
+    if sim == "2":
+        natural_selection.run()
 
 
 if __name__ == "__main__":
